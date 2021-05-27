b87d27ddb04a39a9fa8fa34a9238a635451d515f
AspirineHD <47499678+AspirineHD@users.noreply.github.com>
2021-05-27 16:58:10 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <div><Manufacturer /></div>
+  <div><Devices /></div> -->
+  <div class="container">
+    <manufacturer v-slot="data">
+      <devices :manufacturerId="data.factId"></devices>
+    </manufacturer>
+  </div>
+</template>
+
+<script>
+import Manufacturer from "@/components/Manufacturer.vue";
+import Devices from "@/components/Devices.vue";
+export default {
+  components: {
+    Manufacturer: Manufacturer,
+    Devices: Devices,
+  },
+};
+</script>
+
+<style>
+</style>
